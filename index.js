4f8d3d99c9426307531027c95724e0bddb277374
SebastianDelima <53838893+SebastianDelima@users.noreply.github.com>
2019-08-06 17:35:29 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-var greeting = "";
+var greeting = "Hello, Everybody";
 
 var specialGuest = "Neil deGrasse Tyson"
 
-var greetSpecialGuest = "" + specialGuest + "!";
+var greetSpecialGuest = "" + "Neil deGrasse Tyson" + "!";
 
 var topic = "space";
 
-var conversation = `${topic}`;
+var conversation = `${Do you like stars}`;
